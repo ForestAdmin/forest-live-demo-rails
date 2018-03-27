1de515ae07634edacf328820deaaf14a37c3d320
Sandro Munda <sandro@munda.me>
2018-03-27 23:24:08 +0200

    Add the 'import data' Smart Action on Product

--- app/controllers/forest/products_controller.rb
+++ app/controllers/forest/products_controller.rb
@@ -1,3 +1,5 @@
+require 'data_uri'
+
 class Forest::ProductsController < ForestLiana::ApplicationController
 
   def buyers
@@ -9,5 +11,29 @@ class Forest::ProductsController < ForestLiana::ApplicationController
     count = orders.count
 
     render json: serialize_models(customers, include: ['address'], count: count)
+  end
+
+  def import_data
+    uri = URI::Data.new(params.dig('data', 'attributes', 'values', 'CSV file'))
+
+    CSV.parse(uri.data, { col_sep: ';' }).each do |row|
+      price = 0
+      case params.dig('data', 'attributes', 'values', 'Type')
+      when 'phone'
+        price = rand(300..1000) * 100
+      when 'dress'
+        price = rand(10..200) * 100
+       when 'toy'
+         price = rand(5..100) * 100
+      end
+
+      Product.create({
+        label: row[0],
+        price: price,
+        picture: row[1].gsub('//i5.walmartimages.com/asr/', "//s3-#{ENV['S3_REGION']}.amazonaws.com/#{ENV['S3_BUCKET']}/livedemo/"),
+      })
+    end
+
+    render json: { success: 'Data successfuly imported!' }
   end
 end
--- lib/forest_liana/collections/product.rb
+++ lib/forest_liana/collections/product.rb
@@ -5,6 +5,19 @@ class Forest::Product
 
   has_many :buyers, type: ['String'], reference: 'Customer.id'
 
+  action 'Import data', endpoint: '/forest/products/actions/import-data', type: 'global', fields: [{
+    field: 'CSV file',
+    description: 'A semicolon separated values file stores tabular data (numbers and text) in plain text',
+    type: 'File',
+    isRequired: true
+  }, {
+    field: 'Type',
+    description: 'Specify the product type to import',
+    type: 'Enum',
+    enums: ['phone', 'dress', 'toy'],
+    isRequired: true
+  }]
+
   segment 'Bestsellers' do
     products = ActiveRecord::Base.connection.execute("""
       SELECT products.id, COUNT(orders.*)
