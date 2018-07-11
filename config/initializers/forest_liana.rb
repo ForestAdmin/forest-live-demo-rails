4b507c0b3a0472765f80e06b047b84301881722c
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-07-11 10:16:11 +0200

    adding initializer for Mixpanel + new gem to make it work in the Gemfile

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -5,5 +5,13 @@ ForestLiana.integrations = {
   stripe: {
     api_key: ENV['STRIPE_SECRET_KEY'],
     mapping: 'Customer.stripe_id'
+  },
+  mixpanel: {
+    api_key: 'b6195a0e1324007c9d596bc70d15d3a5',
+    api_secret: 'cf8e1c69f540f109318d16aea2afea10',
+    mapping: ['Customer.email', 'Company.name'],
+    custom_properties: ['Campaign Source', 'plan', 'tutorial complete'],
   }
 }
+
+
