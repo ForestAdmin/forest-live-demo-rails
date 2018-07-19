01cc23cd572d0837927708483b10680e6a512ea9
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-07-19 10:28:05 +0200

    Fix the permissions with the livedemo

--- app/decorators/controllers/forest_liana/application_controller.rb
+++ app/decorators/controllers/forest_liana/application_controller.rb
@@ -2,6 +2,8 @@ ForestLiana::ApplicationController.class_eval do
   def authenticate_user_from_jwt
     # This is a public live-demo project. That's why we bypass the Forest
     # Liana authentication method here.
+
+    # TODO: Get rid of this dirty hack and implement a real authentication for the livedemo
+    @rendering_id = 23065
   end
 end
-
