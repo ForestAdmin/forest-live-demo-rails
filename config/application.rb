8ed208a6711019bf92495c5b17574cd236b7b6e8
Sandro Munda <sandro@munda.me>
2018-09-12 15:36:42 +0200

    Add a Kanban and Checklist smart views on new Patients collections

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module LiveDemoRails
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'app.forestadmin.com'
         resource '*', headers: :any, methods: :any,
         expose: ['Content-Disposition']
       end
