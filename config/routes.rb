1f490a28edb898d8c0a47b9798a2f746d4565ed1
Philippe Gadiou <32190905+pgadiou@users.noreply.github.com>
2019-07-22 17:07:55 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 
     get '/CustomerStat' => 'customer_stats#index'
 
-
     get '/LegalDoc' => 'legal_docs#index'
     get '/LegalDoc/:id' => 'legal_docs#show'
     put '/LegalDoc/:id' => 'legal_docs#update'
