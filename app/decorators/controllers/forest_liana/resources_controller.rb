b7764cd1abb90178048e0e7fb378ef6a99be4fd7
Steve <bunlon.steve@gmail.com>
2020-09-18 12:06:15 +0200

    add back live demo restrictions

---
+++ app/decorators/controllers/forest_liana/resources_controller.rb
@@ -0,0 +1,21 @@
+# Prevent to change data on our public Live Demo :-)
+ForestLiana::ResourcesController.class_eval do
+  unless Rails.env.development?
+    caches_action :index, cache_path: Proc.new { |c| c.request.url }
+  end
+
+  def create
+    render body: 'You can only read data on this public demo application.',
+      status: 403
+  end
+
+  def update
+    render body: 'You can only read data on this public demo application.',
+      status: 403
+  end
+
+  def destroy
+    render body: 'You can only read data on this public demo application.',
+      status: 403
+  end
+end
